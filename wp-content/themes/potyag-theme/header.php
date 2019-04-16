850d9647f452d172008d4561f4b7ad4b577bc6f2
Muratis <kaznikolas12@gmail.com>
2019-04-16 22:05:07 +0300

    init wp

--- wp-content/themes/potyag-theme/header.php
+++ wp-content/themes/potyag-theme/header.php
@@ -75,6 +75,7 @@
 									<li><a href="#our-tobacco" class="scroll-to">Галерея</a></li>
 									<li><a href="#testimonials" class="scroll-to">Відгуки</a></li>
 									<li><a href="#address-map" class="scroll-to">Контакти</a></li>
+									<li><a href="/menu" class="scroll-to">Меню</a></li>
 								</ul>
 								<div class="fix-content">
 									<div class="mob-phone-wrapper">
@@ -106,6 +107,7 @@
 									<li><a href="#our-tobacco" class="scroll-to">Галерея</a></li>
 									<li><a href="#testimonials" class="scroll-to">Відгуки</a></li>
 									<li><a href="#address-map" class="scroll-to">Контакти</a></li>
+									<li><a href="/menu" class="scroll-to">Меню</a></li>
 								</ul>
 							</div>
 						</div>
@@ -113,54 +115,7 @@
 				</div>
 			</nav>
 		</div>
-
-		<div class="header-content">
-<!--			<video id="myVideo" loop="" autoplay="" preload  width="100%" height="100%" >-->
-<!--				<source src="/wp-content/themes/potyag-theme/assets/img/dum.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2"'>-->
-<!--			</video>-->
-			<!--                <div class="img-overlay"></div>-->
-
-				<div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
-					<div class="container-banner">
-						<div class="row">
-							<div class="content content-text col-md-12">
-								
-
-								<!--                            <img src="/img/header-mob-pic.png" alt="" class="mob-pic">-->
-								<!--                            <h1>Кальян-бар <br> "Потяг"</h1>-->
-								<img src="/wp-content/themes/potyag-theme/assets/img/banner.png" alt="banner" class="banner">
-
-								<a href="#" class="button js-onload" data-toggle="modal" data-target="#quest-modal" >Замовити стіл</a>
-								<!--                            <img src="/img/header-pic.png" alt="" class="pic">-->
-							</div>
-						</div>
-					</div>
-
-					<div class="carousel-inner .carousel-fade">
-					<div class="img-overlay"></div>
-					<div class="carousel-item active">
-						<img class="d-block w-100" src="/wp-content/themes/potyag-theme/assets/img/gallery/gallery3.jpg" alt="First slide">
-					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="/wp-content/themes/potyag-theme/assets/img/gallery/gallery1.jpg" alt="Second slide">
-					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="/wp-content/themes/potyag-theme/assets/img/gallery/gallery4.jpg" alt="Third slide">
-					</div>
-						<div class="carousel-item">
-							<img class="d-block w-100" src="/wp-content/themes/potyag-theme/assets/img/gallery/gallery5.jpg" alt="Third slide">
-						</div>
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div>
-		</div>
+		
 	</header>
 
 	<div class="modal fade" id="quest-modal" role="dialog">
--- wp-content/themes/potyag-theme/page.php
+++ wp-content/themes/potyag-theme/page.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 
-		<div class="content <?php esc_attr_e( $l['content-class'] ); ?>">
+		<div class="content-page <?php esc_attr_e( $l['content-class'] ); ?>">
 			<?php if(have_posts()): while(have_posts()) : the_post(); ?>
 
 				<?php the_content(); ?>
@@ -24,40 +24,10 @@ get_header();
 			<?php endif; ?>
 
 		</div>
-<section class="pre-footer">
-	<div class="container">
-		<div class="row">
-			<div class="content col-md-12">
-				<div class="documents">
-					<a href="/" class="scroll-to">Головна</a>
-					<a href="#about-us" class="scroll-to">Про нас</a>
-					<a href="#our-tobacco" class="scroll-to">Галерея</a>
-					<a href="#our-tobacco" class="scroll-to">Наш табак</a>
-					<a href="#address-map" class="scroll-to">Відгуки</a>
-					<a href="#address-map" class="scroll-to">Контакти</a>
-				</div>
-				<div class="soc-wrapper">
-					<a href="#" class="ofs" target="_blank">&copy; 2019. Potyag-Bar, усі права захищені</a>
-
-				</div>
-			</div>
-		</div>
-	</div>
-	<a href="#" class="go-top up-btn"></a>
-</section>
-
-
-<div class="order-modal-btn">
-	<button class="modal-button"><img src="/wp-content/themes/potyag-theme/assets/img/logo.jpg"></button>
-</div>
-
-</div>
-
-
 
 <?php
 
 
-wp_footer(); ?>
+get_footer() ?>
 </body>
 </html>
